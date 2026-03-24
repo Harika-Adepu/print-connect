05e4fc69417ad2439819d16b192e9a05fc59ea54
Harika-Adepu <b210956@rgukt.ac.in>
2026-03-24 14:56:53 +0530

    App scanned

--- mobile-app/src/config/env.js
+++ mobile-app/src/config/env.js
@@ -2,7 +2,7 @@
 // 👉 Change BASE_URL to your deployed backend URL in production
 
 const ENV = {
-  BASE_URL: "http://192.168.1.100:5001/api", // ← Replace with your local IP when testing on device
+  BASE_URL: "http://192.168.43.67:5001/api", // ← Replace with your local IP when testing on device
   RAZORPAY_KEY_ID: "YOUR_RAZORPAY_KEY_ID",   // ← Replace before payment testing
 };
 
